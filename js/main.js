58fbf3bc43186e360821c112b3b8770190bbae6e
vinescarlan <vinescarlan@gmail.com>
2015-10-06 13:18:40 +0800

    Add src property to other locations

--- js/main.js
+++ js/main.js
@@ -54,31 +54,27 @@ var locations = [
 	{
 		stringLocation: "LBC G/F Araneta Square Rizal Avenue Corner Samson Road Monumento Caloocan, 1400 Metro Manila",
 		nearbyLocations: [1, 2, 3, 4],
-		src: "pb=!1m18!1m12!1m3!1d3860.0021207311065!2d120.98231100000001!3d14.655821000000019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b42bf34f7895%3A0x6a41b8fd8f5cf68d!2sLBC!5e0!3m2!1sen!2sph!4v1444104464368"
+		src: "pb=!1m18!1m12!1m3!1d30880.739952115076!2d120.97355147888182!3d14.650690700246718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x4ac6b2475ba01163!2sLBC!5e0!3m2!1sen!2sph!4v1444108307505"
 	},
 	{
 		stringLocation: "LBC G/F Victory Central Mall Rizal Ave Monumento 072, Caloocan, 1400 Metro Manila",
 		nearbyLocations: [0, 2, 3, 4],
-		lat: 14.655821,
-		lng: 120.982311
+		src: "pb=!1m18!1m12!1m3!1d3860.0021207311065!2d120.98231100000001!3d14.655821000000019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b42bf34f7895%3A0x6a41b8fd8f5cf68d!2sLBC!5e0!3m2!1sen!2sph!4v1444104464368"
 	},
 	{
 		stringLocation: "LBC Asuncion Corner Loreto St. Morning Breeze Subd 84, Caloocan 1400 Metro Manila",
 		nearbyLocations: [0, 1, 3, 4],
-		lat: 14.657591,
-		lng: 120.98757
+		src: "pb=!1m18!1m12!1m3!1d30880.739952115076!2d120.97355147888182!3d14.650690700246718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x2e69fc77e0b8e434!2sLbc!5e0!3m2!1sen!2sph!4v1444108354511"
 	},
 	{
 		stringLocation: "LBC 95 10th Ave. Cor. Heroes 65, Caloocan 1408 Metro Manila",
 		nearbyLocations: [0, 1, 2, 4],
-		lat: 14.651816,
-		lng: 120.977301
+		src: "pb=!1m18!1m12!1m3!1d30880.739952115076!2d120.97355147888182!3d14.650690700246718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x713e4f176a4f9658!2sLBC!5e0!3m2!1sen!2sph!4v1444108399410"
 	},
 	{
 		stringLocation: "LBC 1604 Rizal Ave. Ext. Between 10th And 11th Ave 68, Caloocan 1400 Metro Manila",
 		nearbyLocations: [0, 1, 2, 3],
-		lat: 14.6522169,
-		lng: 120.9836649
+		src: "pb=!1m18!1m12!1m3!1d30880.739952115076!2d120.97355147888182!3d14.650690700246718!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0000000000000000%3A0x15f5ed1109de8d49!2sLBC!5e0!3m2!1sen!2sph!4v1444108435963"
 	}
 ];
 
