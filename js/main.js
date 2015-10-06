d3c4a13ca65bff4af78e86f0eb38cca21ee53af8
vinescarlan <vinescarlan@gmail.com>
2015-10-06 12:15:14 +0800

    Modify locations

--- js/main.js
+++ js/main.js
@@ -53,23 +53,32 @@ lng:
 var locations = [
 	{
 		stringLocation: "LBC G/F Araneta Square Rizal Avenue Corner Samson Road Monumento Caloocan, 1400 Metro Manila",
-		nearbyLocations: [1, 2, 3, 4]
+		nearbyLocations: [1, 2, 3, 4],
+		src: "pb=!1m18!1m12!1m3!1d3860.0021207311065!2d120.98231100000001!3d14.655821000000019!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b42bf34f7895%3A0x6a41b8fd8f5cf68d!2sLBC!5e0!3m2!1sen!2sph!4v1444104464368"
 	},
 	{
 		stringLocation: "LBC G/F Victory Central Mall Rizal Ave Monumento 072, Caloocan, 1400 Metro Manila",
-		nearbyLocations: [0, 2, 3, 4]
+		nearbyLocations: [0, 2, 3, 4],
+		lat: 14.655821,
+		lng: 120.982311
 	},
 	{
 		stringLocation: "LBC Asuncion Corner Loreto St. Morning Breeze Subd 84, Caloocan 1400 Metro Manila",
-		nearbyLocations: [0, 1, 3, 4]
+		nearbyLocations: [0, 1, 3, 4],
+		lat: 14.657591,
+		lng: 120.98757
 	},
 	{
 		stringLocation: "LBC 95 10th Ave. Cor. Heroes 65, Caloocan 1408 Metro Manila",
-		nearbyLocations: [0, 1, 2, 4]
+		nearbyLocations: [0, 1, 2, 4],
+		lat: 14.651816,
+		lng: 120.977301
 	},
 	{
 		stringLocation: "LBC 1604 Rizal Ave. Ext. Between 10th And 11th Ave 68, Caloocan 1400 Metro Manila",
-		nearbyLocations: [0, 1, 2, 3]
+		nearbyLocations: [0, 1, 2, 3],
+		lat: 14.6522169,
+		lng: 120.9836649
 	}
 ];
 
@@ -125,8 +134,8 @@ var pos = 0; // For tracking pHints position
 
 function highlightText() {
 	var pHints = document.querySelectorAll("#search-hints p");
-	if (pHints === undefined || pHints.length === 0	) return false;
-	
+	if (pHints === undefined || pHints.length === 0) return false;
+
 	// Change all pHints background to white
 	for (var i = 0, len = pHints.length; i < len; i++) {
 		pHints[i].style.background = "#fdfdfd";
