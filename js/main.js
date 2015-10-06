fe7e3eb5fee3f99ab3f2f5514d2ba09999dd1d20
vinescarlan <vinescarlan@gmail.com>
2015-10-06 16:36:31 +0800

    Remove unnecessary code

--- js/main.js
+++ js/main.js
@@ -4,52 +4,6 @@ var searchBox = document.getElementById("search-box");
 // Access search button
 var searchBtn = document.getElementById("search-button");
 
-// On each keyup of search box, match the input in list of locations
-var location1 = [
-	// Caloocan
-	"LBC G/F Araneta Square Rizal Avenue Corner Samson Road Monumento Caloocan, 1400 Metro Manila",
-	"LBC G/F Victory Central Mall Rizal Ave Monumento 072, Caloocan, 1400 Metro Manila",
-	"LBC Asuncion Corner Loreto St. Morning Breeze Subd 84, Caloocan 1400 Metro Manila",
-	"LBC 1604 Rizal Ave. Ext. Between 10th And 11th Ave 68, Caloocan 1400 Metro Manila",
-	"LBC 95 10th Ave. Cor. Heroes 65, Caloocan 1408 Metro Manila",
-	// Quezon
-	"LBC L2, Gateway Mall General Roxas Ave Quezon City",
-	"LBC Nice Hotel Building , P. A. Bernardo Cor. Malabito Street, San Martin De Porres , San Martin De Pores , 1111 Quezon City , Metro Manila",
-	"LBC Farmers Market Gen. Araneta Ave Socorro, Quezon City 1109 Metro Manila",
-	"LBC GF Saint Anthony Building 891 Aurora Blvd E. Rodriguez, Quezon City, 1109 Metro Manila",
-	// Manila
-	"LBC Unit 110, 8000 Plaza Residences Lacson Street 303 Sta. Cruz, Manila 1003 Metro Manila",
-	"LBC 624 P. Paterno Street, Quiapo Manila 1001 Metro Manila",
-	"LBC Rizal Avenue 304 Zone 29 Santa Cruz, Manila City 1003 Metro Manila",
-	"LBC Recto Ave 1391, Manila 1008 Metro Manila",
-	"LBC 2Nd Floor Ora Electronics Center 734 Gonzalo Puyat 307, Manila, 1001 Metro Manila",
-	// Mandaluyong
-	"LBC Sierra Centre 8 Libertad Hiway Hills, Mandaluyong 1550 Metro Manila",
-	"LBC Shaw Blvd Mandaluyong 1555 Metro Manila",
-	// Pasig
-	"LBC RN 17 Shaw Boulevard San Antonio, Pasig 1600 Metro Manila",
-	"LBC Unit 9, Roogen Shaw Boulevard San Antonio, Pasig 1609 Metro Manila",
-	// Makati
-	"Lbc Ground Floor Baron Building II 761 Dr Jose P. Rizal St Poblacuon, Makati, 1210 Metro Manila",
-	"LBC Unit 102, MJM Building 650 J.P Rizal Street Makati 1208 Metro Manila",
-	"LBC Makati Avenue Poblacion, Makati 1210 Metro Manila",
-	"LBC GF Jazz Mall N.Garcia St.Former Repuso St. Bel-Air II, Makati, 1209 Metro Manila"
-];
-
-/*
-string location
-coordinates
-nearby location
-[
-{
-stringLocation: ,
-nearbylocation: ,
-lat: , 
-lng:
-}
-]
-
-*/
 var locations = [
 	{
 		stringLocation: "LBC G/F Araneta Square Rizal Avenue Corner Samson Road Monumento Caloocan, 1400 Metro Manila",
